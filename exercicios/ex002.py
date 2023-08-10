f5005c6c71cc89fa54fa899e6bec3da056cf6726
Pedro Neto <pedro.silveira@easychange.com.br>
2023-08-10 18:08:28 -0300

    Inicializando Repositorio de aprendizado em python

---
+++ exercicios/ex002.py
@@ -0,0 +1,5 @@
+# Carregue a data atual do computador e apresente somente a data
+from datetime import datetime
+
+data_atual = datetime.now()
+print(data_atual.date())
---
+++ exercicios/ex003.py
@@ -0,0 +1,4 @@
+# Considere o seguinte dicionário: {'m1': {'m2': 'Olá Mundo'}}. Carregue e apresente a mensagem "Olá Mundo" contida no dicionário
+
+mensagem = {'m1': {'m2': 'Olá Mundo'}}
+print(mensagem['m1']['m2'])
