dda0e930d7f4164bc45f0bcd86d6abad8d53d421
Pedro Neto <pedro.silveira@easychange.com.br>
2023-09-03 16:53:50 -0300

    Aula 02 função print explicada e parametro sep e end apresentados

---
+++ aulas/ayla2.py
@@ -0,0 +1,22 @@
+# A função print é utilizada para exibir no terminal, ela pode receber um ou mais argumento
+
+# Por padrão ele imprime os argumentos, adiciona espaçamento entre eles e quebra de linha
+
+print(12, 34)
+print(56, 78)
+
+#  Aceita parámetros para mudar a separação dos argumentos ( pode ser utilizado '' ou "")
+
+print(12, 34, sep='/')
+print(21, 89, sep="-")
+
+#CRF = /r/n - retorna depois quebra a linha
+# LF = /n - quebra de linha
+
+print(10, 7, sep='-', end='\r\n')
+print(30, 13, sep=' opa ', end='\n')
+print(11, 69, sep='-', end='\n')
+
+
+# Gerando erro no terminal para ver a solução apresentada
+Print()
