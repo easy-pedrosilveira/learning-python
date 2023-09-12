9b9edeb45188510b66f3c97d9e68530a994978a8
Pedro Neto <pedro.silveira@easychange.com.br>
2023-09-11 23:58:13 -0300

    Correção de identação do código

--- aulas/aula12.py
+++ aulas/aula12.py
@@ -3,5 +3,5 @@ altura = 1.80
 peso = 95
 imc = peso / (altura * altura)
 
-print(nome, 'tem', straltura, 'de altura')
+print(nome, 'tem', altura, 'de altura')
 print('pesa', peso, '', altura, 'de altura')
